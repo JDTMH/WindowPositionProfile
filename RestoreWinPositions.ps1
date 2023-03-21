cece93eb8d5dce5f9f63034121f50b19952148b4
JDTMH <124918363+JDTMH@users.noreply.github.com>
2023-03-21 14:11:20 -0400

    Window Position Profile save and restore scripts
    I really lik the program DisplayFusion for the window position profiles.  That's really all I would use it for, I would be able to easily save and restore positions and sizes of open programs, so that when I remove my laptop from the dock, and then put it back, I can quickly get my programs back in place... I'm a little OCD you could say...  I liked that I could add a key combination to quickly restore my positions.
    
    Well, unfortunately, the IT team does NOT like this program, or anything else I have tried up to this point as an alternative such as AquaSnap and quite a few others.  I don't spend a lot of time arguing with them I just remove the program that they ask and move on about my business.
    
    I plan to build a stand alone app in maybe .NET or something that will be visual like DisplayFusion offers for managing and restoring window positions, but I'm in a hurry at the moment and just need something that works for now.  I managed to find a powershell script online, unfortunately I do not recall exactly where the original script came from, that would allow me to record the process.  In theory, this worked for most things, but when you are running multiple windows of the same program, it would get confused and sometimes either not restore them, or put them in the incorrect position.
    
    So I made some adjustments to the original code so that it will look instead at the window title and save the information and then restore from there.  It's primitive, but it seems to work for what I need right now.  But I wanted to also share it out there incase someone is in a similar position where IT keeps gunning for them.
    
    Right now you have to manually type in the names of the windows into the save script, but I hope to make adjustments so that there will be a choice box to choose open apps instead.  But for now, I want to share what I have with the world.

---
+++ RestoreWinPositions.ps1
@@ -0,0 +1,84 @@
+﻿<#
+The purpose of this script is to allow quick and easy restoration of saved window size and position.
+Author:JDTMH
+Date: 03/21/2023
+To do: 
+#>
+
+
+
+#Reads json file into a configuration object 
+Function ParseConfig($configpath)
+{
+  $jsontmp = Get-Content $configpath | Out-String | ConvertFrom-Json
+  $global:config = {$jsontmp}.Invoke()
+}
+
+#Modifies a given window to a given size and position using the Windows API
+Function MoveWindow($windowjson)
+{
+  $window = $windowjson | ConvertFrom-Json
+  # Extract the name from the handle object using regex
+  $windowName = $handle.Name -replace '^.*?\((.*?)\)$', '$1'
+  Write-Host "Searching for program's with the following name: $windowName"
+  $handle = Get-Process | Where-Object {$_.MainWindowTitle -like "*$($window.windowname)*"} | Select-Object -First 1
+
+  #Select the program instance with a valid window 
+  foreach ($h in $handle)
+  {
+      if ($h.MainWindowHandle -ne 0) {
+        $takeHandle = $h.MainWindowHandle
+        break
+      }
+  }
+
+  #Skip if the program is not currently running
+ if($takeHandle) {
+    # Extract the name from the handle object using regex
+    $windowName = $handle.Name -replace '^.*?\((.*?)\)$', '$1'
+    Write-Host "Moving Program $windowName"
+    $return = [Window]::MoveWindow($takeHandle, $window.x, $window.y, $window.width, $window.height, $True)
+    Write-Host "Success?" $return "`n"
+  }
+  else {
+    Write-Host "Program $windowName not running!"
+  }
+}
+
+# =======
+#  MAIN
+# =======
+
+#Window object type definition to interact with the Windows API
+Try{
+      [void][Window]
+} 
+Catch {
+  Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Window {
+  [DllImport("user32.dll")]
+  [return: MarshalAs(UnmanagedType.Bool)]
+  public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+  [DllImport("User32.dll")]
+  public extern static bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+}
+public struct RECT
+{
+  public int Left;        // x position of upper-left corner
+  public int Top;         // y position of upper-left corner
+  public int Right;       // x position of lower-right corner
+  public int Bottom;      // y position of lower-right corner
+}
+"@
+}
+
+$global:config = New-Object System.Collections.ArrayList
+#Reading file with configured size and position values
+#Use `SaveWinPositions.ps1` to record these values
+$configpath = $env:USERPROFILE + "\windowlayout.config"
+
+#Modify windows' size and position as spec'd in the configuration file
+ParseConfig($configpath)
+$global:config | ForEach-Object { MoveWindow($_) }
---
+++ SaveWinPositions.ps1
@@ -0,0 +1,86 @@
+﻿<#
+The purpose of this script is to allow quick and easy recording of current window size and position.
+Author:JDTMH
+Date: 03/21/2023
+To do: 
+#>
+
+
+#Saves configuration object data to a json file
+Function SaveConfig($configpath)
+{
+  $global:config | ConvertTo-Json | Out-File $configpath
+}
+#Queries the Windows API for a given window's size and position
+#Saves it to a configuration object
+Function GetWindowData($windowName)
+{
+  $matchingWindows = Get-Process | Where-Object {$_.MainWindowTitle -like "*$windowName*"}
+if ($matchingWindows.Count -gt 0) {
+    $takeHandle = $matchingWindows[0].MainWindowHandle
+}
+else {
+    Write-Host "Program matching '$windowName' not found"
+    return
+}
+
+  #Skip if the program is not found
+  if($takeHandle -ne 0) {
+    Write-Host "Recording data for window $windowName-$takeHandle ..."
+
+    # This is to get the current window state
+    $Rectangle = New-Object RECT
+    $Return = [Window]::GetWindowRect($takeHandle,[ref]$Rectangle)
+    $windowObject = New-Object -TypeName psobject
+    $windowObject | Add-Member -MemberType NoteProperty -Name windowname -Value $windowName
+    $windowObject | Add-Member -MemberType NoteProperty -Name x -Value $Rectangle.Left
+    $windowObject | Add-Member -MemberType NoteProperty -Name y -Value $Rectangle.Top
+    $windowObject | Add-Member -MemberType NoteProperty -Name width -Value ($Rectangle.Right - $Rectangle.Left)
+    $windowObject | Add-Member -MemberType NoteProperty -Name height -Value ($Rectangle.Bottom - $Rectangle.Top)
+    $json = $windowObject | ConvertTo-Json
+    $global:config.Add($json)
+  }
+  else {
+    Write-Host "Window $windowName not found"
+  }
+}
+
+
+# =======
+#  MAIN
+# =======
+
+#Window object type definition to interact with the Windows API
+Try{
+  [void][Window]
+} 
+Catch {
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Window {
+[DllImport("user32.dll")]
+[return: MarshalAs(UnmanagedType.Bool)]
+public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+[DllImport("User32.dll")]
+public extern static bool MoveWindow(IntPtr handle, int x, int y, int width, int height, bool redraw);
+}
+public struct RECT
+{
+public int Left;        // x position of upper-left corner
+public int Top;         // y position of upper-left corner
+public int Right;       // x position of lower-right corner
+public int Bottom;      // y position of lower-right corner
+}
+"@
+}
+
+$global:config = New-Object System.Collections.ArrayList
+$configpath = $env:USERPROFILE + "\windowlayout.config"
+
+#Creating an array with the window names to record size and position
+$windows = "Teams","Outlook","Vivaldi", "Notepad++", "Active Directory Users and Computers", "DHCP", "Microsoft Endpoint Configuration Manager", "OneNote", "Excel", "Word", "Spotify", "PDQ Deploy", "PDQ Inventory"
+
+#Read and record window sizes and positions
+$windows | ForEach-Object { GetWindowData($_) }
+SaveConfig($configpath)
